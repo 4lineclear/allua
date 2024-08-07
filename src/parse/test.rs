0f4874022158e0e6c784ba8db2ddf9e8d54208cf
4lineclear <tamimhakimi21@gmail.com>
2024-08-07 18:58:36 -0400

    Changed parsed token testing

--- src/parse/test.rs
+++ src/parse/test.rs
@@ -1,48 +1,76 @@
 // TODO: create custom tester
-use expect_test::expect;
-
+// use expect_test::expect;
 use super::Reader;
+use pretty_assertions::assert_eq;
 
 use write::*;
 mod write;
 
 const PUNCT_SRC: &str = "}()[],.@#~?:$=!<>-&|+*/^%";
 
+macro_rules! pos {
+    () => {
+        (file!(), line!(), column!())
+    };
+}
+
+fn map_errs(s: &str) -> String {
+    let mut errs = String::with_capacity(s.len());
+    s.trim()
+        .lines()
+        .map(str::trim)
+        .map(str::trim)
+        .for_each(|s| {
+            errs.push_str(s);
+            errs.push('\n');
+        });
+    if errs.chars().last() == Some('\n') {
+        errs.pop();
+    }
+    errs
+}
+
 macro_rules! do_test {
-    ($src:expr, $expected_tokens:literal, $expected_errors:literal $(,)?) => {{
+    ($src:expr, $expected_tokens:expr, $expected_errors:expr $(,)?) => {{
         let (module, errors) = Reader::new($src).module("test");
         // extra info here in case of failure
-        println!("{module:#?}");
-
-        expect![[$expected_tokens]].assert_eq(&write_module($src, &module));
-        expect![[$expected_errors]].assert_eq(&write_errs($src, &errors));
+        println!("testing {:?} {module:#?}", pos!());
+        let errs = map_errs($expected_errors);
+        assert_eq!(
+            $expected_tokens
+                .into_iter()
+                .map(str::to_owned)
+                .collect::<Vec<_>>(),
+            write_module($src, &module)
+        );
+        assert_eq!(&errs, &write_errs($src, &errors));
     }};
 }
 
 #[test]
 fn empty() {
-    do_test!("", "", r"");
+    do_test!("", [], "");
 }
 
 #[test]
 fn unexpected_punct() {
     do_test!(
         PUNCT_SRC,
-        "",
+        [],
         r#"unexpected 0,25 = "}()[],.@#~?:$=!<>-&|+*/^%""#,
     );
 }
 
 #[test]
 fn unclosed_block_comment() {
-    do_test!("/*/*/**/*/", "", r#"unclosed 0,10 = "/*/*/**/*/""#);
+    do_test!("/*/*/**/*/", [], r#"unclosed 0,10 = "/*/*/**/*/""#);
 }
 
 #[test]
 fn let_punct_fail() {
     do_test!(
         &("let ".to_owned() + PUNCT_SRC),
-        "",
+        [],
         r#"
             unexpected 4,29 = "}()[],.@#~?:$=!<>-&|+*/^%"
             eof 29"#,
@@ -55,7 +83,7 @@ fn multi_err() {
         "\
         let aa = // \n\
         /**/ ^@@ # !/*/*/**/*/",
-        "let aa",
+        ["let", "aa"],
         r##"
             unexpected 18,21 = "^@@"
             unexpected 22,23 = "#"
@@ -67,21 +95,32 @@ fn multi_err() {
 
 #[test]
 fn decl() {
-    do_test!("let yeah = 3;", "let yeah = 3", "");
-    do_test!("const yeah = 3;", "const yeah = 3", "")
+    do_test!("let yeah = 3;", ["let", "yeah", "=", "3"], "");
+    do_test!("const yeah = 3;", ["const", "yeah", "=", "3"], "");
 }
 
 #[test]
 fn decl_with_type() {
-    do_test!("let string yeah = 3;", "let string yeah = 3", "",);
-    do_test!("const string yeah = 3;", "const string yeah = 3", "",)
+    do_test!(
+        "let string yeah = 3;",
+        ["let", "string", "yeah", "=", "3"],
+        ""
+    );
+    do_test!(
+        "const string yeah = 3;",
+        ["const", "string", "yeah", "=", "3"],
+        ""
+    );
 }
 
 #[test]
 fn let_chain() {
     do_test!(
         &"let yeah = 3;".repeat(5),
-        "let yeah = 3let yeah = 3let yeah = 3let yeah = 3let yeah = 3",
+        [
+            "let", "yeah", "=", "3", "let", "yeah", "=", "3", "let", "yeah", "=", "3", "let",
+            "yeah", "=", "3", "let", "yeah", "=", "3"
+        ],
         "",
     )
 }
@@ -93,107 +132,87 @@ fn let_and_fn() {
         let yeah = 3;\n\
         print(yeah);\
         ",
-        "let yeah = 3print(yeah)",
+        ["let", "yeah", "=", "3", "print", "(", "yeah", ")",],
         "",
     );
 }
 
 #[test]
 fn fn_call_str() {
-    do_test!(r#"print("yeah");"#, r#"print("yeah")"#, "");
+    do_test!(r#"print("yeah");"#, ["print", "(", "\"yeah\"", ")"], "");
 }
 
 #[test]
 fn fn_fail_single_param() {
-    do_test!(r#"print("""#, r"", "eof 8");
-    do_test!(r#"print("#, r"", "eof 6");
-    do_test!(r#"print(print"#, r"", "eof 11");
+    do_test!(r#"print("""#, [], "eof 8");
+    do_test!(r#"print("#, [], "eof 6");
+    do_test!(r#"print(print"#, [], "eof 11");
     do_test!(
         r#"print(print("#,
-        r"",
+        [],
         r#"
         eof 12
         eof 12"#,
     );
-    do_test!(r#"print"#, r"", r#"unexpected 0,5 = "print""#);
+    do_test!(r#"print"#, [], r#"unexpected 0,5 = "print""#);
 }
 
 #[test]
 fn fn_fail_multi_param() {
-    do_test!(r#"print("yeah","""#, r"", "eof 15");
-    do_test!(r#"print(one"#, r"", "eof 9");
-    do_test!(r#"print(yeah, yeah(), """#, r"", r#"eof 22"#);
+    do_test!(r#"print("yeah","""#, [], "eof 15");
+    do_test!(r#"print(one"#, [], "eof 9");
+    do_test!(r#"print(yeah, yeah(), """#, [], r#"eof 22"#);
     do_test!(
         r#"print(print(), print("#,
-        r"",
+        [],
         r#"
             eof 21
             eof 21"#,
     );
-    do_test!(r#"print(yeah, 1, print"#, r"", "eof 20");
+    do_test!(r#"print(yeah, 1, print"#, [], "eof 20");
 }
 
 #[test]
 fn nested_fn() {
-    do_test!(r#"print(print());"#, "print(print())", r#""#);
+    do_test!(
+        r#"print(print());"#,
+        ["print", "(", "print", "(", ")", ")"],
+        r#""#
+    );
     do_test!(
         r#"print(print(), print());"#,
-        "print(print(), print())",
-        r#""#,
+        ["print", "(", "print", "(", ")", ",", "print", "(", ")", ")"],
+        "",
     );
     do_test!(
         r#"print(print(print(print(print(print(print()))))));"#,
-        "print(print(print(print(print(print(print()))))))",
-        r#""#,
+        [
+            "print", "(", "print", "(", "print", "(", "print", "(", "print", "(", "print", "(",
+            "print", "(", ")", ")", ")", ")", ")", ")", ")"
+        ],
+        "",
     );
     do_test!(
         r#"print(print(print(), ""), print(print(one, two, three, yeah(five))));"#,
-        r#"print(print(print(), ""), print(print(one, two, three, yeah(five))))"#,
-        r#""#,
+        [
+            "print", "(", "print", "(", "print", "(", ")", ",", "\"\"", ")", ",", "print", "(",
+            "print", "(", "one", ",", "two", ",", "three", ",", "yeah", "(", "five", ")", ")", ")",
+            ")",
+        ],
+        "",
     );
 }
 
 #[test]
 fn block() {
-    do_test!(
-        "{}",
-        r#"
-        {
-        }
-    "#,
-        r"",
-    );
-    do_test!(
-        "{print(yeah);}",
-        r#"
-        {
-            print(yeah)
-        }
-    "#,
-        r"",
-    );
-    do_test!(
-        "{{}}",
-        r#"
-        {
-            {
-            }
-        }
-    "#,
-        r"",
-    );
+    do_test!("{}", ["{", "}"], "",);
+    do_test!("{print(yeah);}", ["{", "print", "(", "yeah", ")", "}"], "",);
+    do_test!("{{}}", ["{", "{", "}", "}"], "",);
     do_test!(
         "{{print(yeah);}}",
-        r#"
-        {
-            {
-                print(yeah)
-            }
-        }
-    "#,
-        r"",
+        ["{", "{", "print", "(", "yeah", ")", "}", "}"],
+        "",
     );
-    // FIX: printing doesn't work as it should
     do_test!(
         r#"{
             {
@@ -201,14 +220,7 @@ fn block() {
                 print(yeah);
             }
         }"#,
-        r#"
-        {
-            {
-                let string yeah = ""
-                print(yeah)
-            }
-        }
-    "#,
-        r"",
+        ["{", "{", "let", "string", "yeah", "=", "\"\"", "print", "(", "yeah", ")", "}", "}"],
+        "",
     );
 }
--- src/parse/test/write.rs
+++ src/parse/test/write.rs
@@ -4,124 +4,104 @@ use crate::{
     util::Symbol,
 };
 
-use std::fmt::{self, Write};
-type Result = std::result::Result<bool, fmt::Error>;
-
 #[derive(Debug)]
 struct Writer<'a> {
     #[allow(dead_code)]
     src: &'a str,
     items: &'a [Token],
     pos: u32,
-    out: &'a mut String,
+    out: &'a mut Vec<String>,
     blocks: Vec<u32>,
 }
 
 impl<'a> Writer<'a> {
-    fn write_block_end(&mut self) -> Result {
-        while self.blocks.last() == Some(&self.pos) {
-            self.blocks.pop();
-            if !self.out.ends_with('\n') {
-                write!(self.out, "\n")?;
-            }
-            write!(self.out, "{:width$}", "", width = self.blocks.len() * 4)?;
-            write!(self.out, "}}\n")?;
-        }
-        Ok(true)
+    fn push(&mut self, s: impl Into<AS<'a>>) {
+        self.out.push(match s.into() {
+            AS::Str(s) => s.to_owned(),
+            AS::String(s) => s,
+            AS::Symbol(s) => s.as_str().to_owned(),
+        })
     }
-    fn write_token(&mut self, token: Token) -> Result {
-        if self.out.get(self.out.len().saturating_sub(2)..) == Some(" \n") {
-            self.out.remove(self.out.len() - 2);
-        }
-        write!(self.out, "{:width$}", "", width = self.blocks.len() * 4)?;
 
-        let cont = match token {
+    fn write_block_end(&mut self) {
+        while self.blocks.last() == Some(&(self.pos + 1)) {
+            self.blocks.pop();
+            self.push("}");
+        }
+    }
+
+    fn write_token(&mut self, token: Token) {
+        match token {
             Token::Fn(_) => todo!("fns not added yet"),
             Token::Decl(decl) => {
-                match decl.kind {
-                    DeclKind::Let => write!(self.out, "let")?,
-                    DeclKind::Const => write!(self.out, "const")?,
-                };
+                self.push(match decl.kind {
+                    DeclKind::Let => "let",
+                    DeclKind::Const => "const",
+                });
+
                 if let Some(type_name) = decl.type_name {
-                    write!(self.out, " {}", type_name.as_str())?;
+                    self.push(type_name.as_str());
                 }
-                write!(self.out, " {}", decl.name.as_str())?;
+                self.push(decl.name.as_str());
 
                 if let Some(expr) = decl.value {
-                    write!(self.out, " = ")?;
-                    self.write_expr(expr)?;
+                    self.push("=");
+                    self.write_expr(expr);
                 }
-
-                Ok(true)
             }
             Token::Expr(expr) => self.write_expr(expr),
             Token::Value(val) => self.write_val(val),
             Token::Import(_) => todo!("imports not added yet"),
             Token::Block(span) => {
-                self.out.write_str("{\n")?;
+                self.push("{");
                 self.blocks.push(span.to);
-                Ok(true)
             }
             Token::Dummy => {
-                writeln!(self.out, "dummy")?;
-                Ok(true)
+                self.push("dummy");
             }
-        }?;
-        self.write_block_end()?;
+        };
 
-        Ok(cont)
+        self.write_block_end();
     }
 
-    fn write_expr(&mut self, expr: Expr) -> Result {
+    fn write_expr(&mut self, expr: Expr) {
         match expr {
             Expr::FnCall(name, param_span) => {
-                write!(self.out, "{}(", name.as_str())?;
+                self.push(name.as_str());
+                self.push("(");
                 if !param_span.is_empty() {
                     while self.pos + 1 < param_span.to {
                         self.pos += 1;
                         let token = self.items[self.pos as usize];
-                        let cont = match token {
+                        match token {
                             // prevent infinite recursion
                             _ if token == Token::Expr(expr) => {
                                 panic!("same expr at index {} found: '{expr:#?}'", self.pos)
                             }
                             Token::Expr(expr) => self.write_expr(expr),
                             _ => self.write_token(token),
-                        }?;
-                        if !cont {
-                            return Ok(false);
-                        }
-                        write!(self.out, ", ")?;
+                        };
+                        self.push(",");
                     }
-                }
-
-                if self.out.get(self.out.len().saturating_sub(2)..) == Some(", ") {
-                    self.out.pop();
                     self.out.pop();
                 }
-
-                write!(self.out, ")")?;
-                Ok(true)
+                self.push(")");
             }
             Expr::Var(name) => self.write_var(name),
             Expr::Value(val) => self.write_val(val),
-        }?;
-
-        Ok(true)
+        };
     }
-    fn write_var(&mut self, name: Symbol) -> Result {
-        self.out.write_str(name.as_str())?;
-        Ok(true)
+    fn write_var(&mut self, name: Symbol) {
+        self.push(name);
     }
 
-    fn write_val(&mut self, val: Value) -> Result {
-        self.out.write_str(val.value.as_str())?;
-        Ok(true)
+    fn write_val(&mut self, val: Value) {
+        self.push(val.value.as_str());
     }
 }
 
-pub fn write_module(src: &str, module: &Module) -> String {
-    let mut out = String::new();
+pub fn write_module(src: &str, module: &Module) -> Vec<String> {
+    let mut out = Vec::new();
     let mut writer = Writer {
         src,
         items: &module.items,
@@ -134,19 +114,10 @@ pub fn write_module(src: &str, module: &Module) -> String {
         let Some(&token) = module.items.get(writer.pos as usize) else {
             break;
         };
-
-        if !writer.write_token(token).unwrap() {
-            break;
-        }
+        writer.write_token(token);
         writer.pos += 1;
     }
 
-    writer.write_block_end().unwrap();
-
-    if out.get(out.len().saturating_sub(2)..) == Some(" \n") {
-        out.pop();
-        out.pop();
-    }
     out
 }
 
@@ -183,4 +154,26 @@ pub fn write_errs(src: &str, errs: &ErrorMulti) -> String {
         out.pop();
     }
     out
+}
+
+enum AS<'a> {
+    Str(&'a str),
+    String(String),
+    Symbol(Symbol),
+}
+
+impl<'a> From<&'a str> for AS<'a> {
+    fn from(value: &'a str) -> Self {
+        Self::Str(value)
+    }
+}
+impl<'a> From<String> for AS<'a> {
+    fn from(value: String) -> Self {
+        Self::String(value)
+    }
+}
+impl<'a> From<Symbol> for AS<'a> {
+    fn from(value: Symbol) -> Self {
+        Self::Symbol(value)
+    }
 }
